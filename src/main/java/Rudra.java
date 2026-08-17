6f64b5069f8e760553de9fc79e2515271e1e8428
Chetan <chetangips@gmail.com>
2026-08-17 17:39:05 +0800

    Changed Name from Duke to Rudra

---
+++ src/main/java/Rudra.java
@@ -0,0 +1,10 @@
+public class Rudra {
+    public static void main(String[] args) {
+        String banner = " ____            _            \n"
+                + "|  _ \\ _   _  __| |_ __ __ _ \n"
+                + "| |_) | | | |/ _` | '__/ _` |\n"
+                + "|  _ <| |_| | (_| | | | (_| |\n"
+                + "|_| \\_\\\\__,_|\\__,_|_|  \\__,_|\n";
+        System.out.println(banner);
+    }
+}
